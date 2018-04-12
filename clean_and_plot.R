43ad89000c4f8aa1c054f318f07fafc82ea3c20c
AndresMiguelReyes <areyes43@ucmerced.edu>
2018-04-12 16:14:55 -0700

    Created two for loops to programatically save any possible expected time until absorption graphs in an ".eps" format.

--- clean_and_plot.R
+++ clean_and_plot.R
@@ -2,6 +2,25 @@ expected_time = read.csv("expected_time.csv")
 expected_time = expected_time[,2:ncol(expected_time)]
 names(expected_time) = seq(1,40)
 
-state = 30
+state = 1
 plot_time_vert(expected_time,state)
 plot_time_horz(expected_time,state)
+
+for(i in 1:40)
+{
+  setEPS()
+  title_str = paste("plots/horz_",i,".eps",sep="")
+  postscript(title_str)
+  plot_time_horz(expected_time,i) 
+  dev.off()
+}
+
+# probably could do both graphing in one for loop, but I'll use two to isolate any potential errors
+for(i in 1:40)
+{
+  setEPS()
+  title_str = paste("plots/vert_",i,".eps",sep="")
+  postscript(title_str)
+  plot_time_vert(expected_time,i) 
+  dev.off()
+}
